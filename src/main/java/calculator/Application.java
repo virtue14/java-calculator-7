a71245d62f4e2f6c0becc7efafa90fc477698a67
악덕 <rudejrqkr97@gmail.com>
2024-10-21 22:51:38 +0900

    feat: 빈 문자열 및 null 입력 처리
    입력된 문자열이 빈 문자열("")이거나 null인 경우 0을 반환하는 기능을 추가했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        System.out.println("덧셈할 문자열을 입력해 주세요: ");
         String input = Console.readLine();
 
         int result = add(input);
