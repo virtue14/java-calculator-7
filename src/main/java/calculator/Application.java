15906b5580f7b958c8b10a057a6044e56f977559
악덕 <rudejrqkr97@gmail.com>
2024-10-21 22:37:24 +0900

    feat: 기본 덧셈 기능 구현(쉼표 구분자)
    쉼표(,)를 구분자로 사용하여 입력된 숫자들을 더하는 기본 덧셈 기능을 구현했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        String input = Console.readLine();
+
+        int result = add(input);
+        System.out.print("결과: " + result);
+    }
+
+    public static int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = ",";
+        String[] tokens = input.split(delimiter);
+        int sum = 0;
+
+        for (String token : tokens) {
+            sum += toInt(token);
+        }
+
+        return sum;
+    }
+
+    private static int toInt(String token) {
+        return Integer.parseInt(token);
     }
 }
